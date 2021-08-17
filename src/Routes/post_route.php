aa6e6af0ec7927a61e2a59935383a0bcff782934
orhanurullah <orhann.1512@gmail.com>
2021-08-17 22:46:54 +0300

    I think this project exactly

--- src/Routes/post_route.php
+++ src/Routes/post_route.php
@@ -2,6 +2,7 @@
 namespace Orti\Blogger\Routes;
 
 use Illuminate\Support\Facades\Route;
+use Orti\Blogger\Controllers\PostController;
 
 Route::group(['middleware' => ['web']], function(){
     Route::group(['prefix' => 'post'], function(){
